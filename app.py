82797873ceefd9ba1933ce1bf5f84ac3f1283b5f
苗卫伟 <weiweimiao.mww@alibaba-inc.com>
2021-11-08 13:50:46 +0800

    fix

--- app.py
+++ app.py
@@ -290,11 +290,16 @@ def main(types=None, speed=None, ss_count=None, area=None, exclude_area=None, ip
 
     url_proxy_dic = {
         "https://free.kingfu.cf/clash/proxies": False,
-        "https://free.dswang.ga/clash/proxies": False,
-        "https://proxypool.fly.dev/clash/proxies": False,
-        "https://www.linbaoz.com/clash/proxies": False,
+        "https://proxy.51798.xyz/clash/proxies": True,
         "https://hello.stgod.com/clash/proxies": False,
-        "https://proxy.51798.xyz/clash/proxies": True
+        "https://proxypool.fly.dev/clash/proxies": False,
+        "https://fq.lonxin.net/clash/proxies": False,
+        "https://free.dswang.ga/clash/proxies": False,
+        "https://sspool.herokuapp.com/clash/proxies": False,
+        "https://www.qunima.cc/clash/proxies": False,
+        "https://ss.dswang.ga:8443/clash/proxies": False,
+        "http://8.135.91.61/clash/proxies": False,
+        "https://www.linbaoz.com/clash/proxies": False
     }
     crawler_urls = build_url(url_proxy_dic, types=types, speed=speed, area=area, exclude_area=exclude_area)
     crawlers = create_ss_pool_crawler_process(uaManager, available_data_queue, crawler_urls)
